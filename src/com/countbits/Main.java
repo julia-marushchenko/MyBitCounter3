d9f2320fce528d66acee6f4f2acd4e6ffc5c0958
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-06-03 01:05:26 +0200

    Add files via upload
    Committing Main class

---
+++ src/com/countbits/Main.java
@@ -0,0 +1,23 @@
+// Java program to count bits in binary number representation
+package com.countbits;
+
+// Main class
+public class Main {
+
+    // Main method to run java program
+    public static void main(String[] args) {
+
+        // Printing how many 1s in binary number 8
+        System.out.println("Bits in number 8: " + Integer.bitCount(8));
+
+        // Printing how many 1s in binary number 17
+        System.out.println("Bits in number 17: " + Integer.bitCount(17));
+
+        // Printing how many 1s in binary number 21
+        System.out.println("Bits in number 21: " + Integer.bitCount(21));
+
+        // Printing how many 1s in binary number 27
+        System.out.println("Bits in number 27: " + Integer.bitCount(27));
+
+    }
+}
